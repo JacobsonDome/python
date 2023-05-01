340f8a6578425e2fbb541f1c19c27ee5b7158f06
Jacob <mattonetjacob@gmail.com>
2023-05-01 19:13:19 +0200

    init python app

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def run():
+    return "ok ok"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=int("3000"), debug=True)
